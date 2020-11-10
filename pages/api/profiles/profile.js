783f4e06627b1cf8aa9fca4bdf57e9e2bbb091dc
cluck-mur <cluck.mur@ahk.jp>
2020-11-11 05:05:01 +0900

    MySql対応の途中。

---
+++ pages/api/profiles/profile.js
@@ -0,0 +1,17 @@
+/**
+ * MySql リモート接続
+ * 
+ * https://vercel.com/guides/deploying-next-and-mysql-with-vercel
+ * からコピー
+ */
+const db = require('../../lib/db')
+const escape = require('sql-template-strings')
+
+module.exports = async (req, res) => {
+  const [profile] = await db.query(escape`
+    SELECT *
+    FROM profiles
+    WHERE id = ${req.query.id}
+  `)
+  res.status(200).json({ profile })
+}

e374c3306489a636cf042ee660366ee3cc87faa1
cluck-mur <cluck.mur@ahk.jp>
2020-11-11 10:40:10 +0900

    MySql対応のための実験

--- pages/staff_login/staff_login.tsx
+++ pages/staff_login/staff_login.tsx
@@ -40,6 +40,11 @@ const StaffLogin = ({}) => {
 };
 
 export const getStaticProps: GetStaticProps = async () => {
+  console.log("$MYSQL_HOST: " + process.env.MYSQL_HOST);
+  console.log("$MYSQL_DATABASE: " + process.env.MYSQL_DATABASE);
+  console.log("$MYSQL_USER: " + process.env.MYSQL_USER);
+  console.log("$MYSQL_PASSWORD: " + process.env.MYSQL_PASSWORD);
+
   return {
     props: {},
   };
